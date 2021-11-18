fc458130a78231eac7c22cc5b2a4c77770951d30
Carlos Molinares <carlos.molinares@austincc.edu>
2021-11-18 10:56:56 -0600

    added fibonacci

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("It's alive!")
+console.log("It's alive!");
+
+const fibo = () => {
+    let first = 0;
+    let second = 1;
+    let sum = 0;
+
+    for (let i = 0; i < 10; i++) {
+        sum = first + second;
+        console.log(sum);
+        first = second;
+        second = sum;
+    }
+}
+
